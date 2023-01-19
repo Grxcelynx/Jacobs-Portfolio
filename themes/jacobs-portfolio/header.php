265e480d3658ed26dc67fc45fe203814a69bdb8b
Grace Birnam <grace.mbirnam@gmail.com>
2023-01-18 16:35:40 -0800

    starting nav

--- themes/jacobs-portfolio/header.php
+++ themes/jacobs-portfolio/header.php
@@ -29,24 +29,12 @@
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$jacobs_portfolio_description = get_bloginfo( 'description', 'display' );
-			if ( $jacobs_portfolio_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $jacobs_portfolio_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
+			?>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'jacobs-portfolio' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'jacobs-portfolio' ); ?> <span class="burger-menu"> &#9776;</span></button>
+
 			<?php
 			wp_nav_menu(
 				array(
