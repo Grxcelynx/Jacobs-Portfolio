940d36b45582920416de2a0e7f1925f88d9099e3
Grace Birnam <grace.mbirnam@gmail.com>
2023-01-20 12:12:51 -0800

    footer started

--- themes/jacobs-portfolio/footer.php
+++ themes/jacobs-portfolio/footer.php
@@ -11,20 +11,61 @@
 
 ?>
 
+<?php 
+    // $footer_logo = get_theme_mod('footer_logo');
+
+    $instagram_url = get_theme_mod('ig_url');
+	$linkedin_url = get_theme_mod('linkedin_url');
+	$github_url = get_theme_mod('github_url');
+	$other_url = get_theme_mod('other_media_url');
+	$other2_url = get_theme_mod('other_media2_url');
+
+    $instagram_icon = get_theme_mod('instagram_icon');
+	$linkedin_icon = get_theme_mod('linkedin_icon');
+	$github_icon = get_theme_mod('github_icon');
+	$other_icon = get_theme_mod('other_media_icon');
+	$other2_icon = get_theme_mod('other_media2_icon');
+?>
+
 	<footer id="colophon" class="site-footer">
+
+		<h2 class="footer-title">Let's work together</h2>
+
+		<a class="email" href="mailto:jacobdifede65@gmail.com">jacobdifede65@gmail.com</a>
+		<!-- SOCIAL MEDIA ROW -->
+		<div class="social-media">
+            <ul>
+			<?php if($linkedin_url && $linkedin_icon):?>
+                    <a href="<?php echo esc_url($linkedin_url)?>"><li><img src="<?php echo esc_url($linkedin_icon)?>" alt="instagram footer icon"></li></a>
+				<?php endif; ?>
+
+				<?php if($github_url && $github_icon):?>
+						<a href="<?php echo esc_url($github_url)?>"><li><img src="<?php echo esc_url($github_icon)?>" alt="twitter footer icon"></li></a>
+					<?php endif; ?>
+
+                <?php if($instagram_url && $instagram_icon):?>
+                    <a href="<?php echo esc_url($instagram_url)?>"><li><img src="<?php echo esc_url($instagram_icon)?>" alt="instagram footer icon"></li></a>
+                <?php endif; ?>
+
+					<?php if($other_url && $other_icon):?>
+						<a href="<?php echo esc_url($other_url)?>"><li><img src="<?php echo esc_url($other_icon)?>" alt="other footer icon"></li></a>
+				<?php endif; ?>
+				
+				<?php if($other2_url && $other2_icon):?>
+						<a href="<?php echo esc_url($other2_url)?>"><li><img src="<?php echo esc_url($other2_icon)?>" alt="other2 footer icon"></li></a>
+				<?php endif; ?>
+            </ul>
+        </div>
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'jacobs-portfolio' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'jacobs-portfolio' ), 'WordPress' );
-				?>
 			</a>
-			<span class="sep"> | </span>
+			<!-- <span class="sep"> | </span> -->
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'jacobs-portfolio' ), 'jacobs-portfolio', '<a href="http://grxcelyn.com">Grace Birnam</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'jacobs-portfolio' ), 'jacobs-portfolio', '<a target="_blank" href="http://grxcelyn.com">Grace Birnam</a>' );
 				?>
 		</div><!-- .site-info -->
+
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
--- themes/jacobs-portfolio/inc/customizer.php
+++ themes/jacobs-portfolio/inc/customizer.php
@@ -11,6 +11,37 @@
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
 function jacobs_portfolio_customize_register( $wp_customize ) {
+
+	$wp_customize->add_section('footer_settings', array(
+        'title' => 'Footer Settings',
+    ));
+
+	$url_inputs = array(
+        array('name'=> 'ig_url', 'label' => 'Instagram Url', 'section' => 'footer_settings', 'default' => ''),
+		array('name'=> 'linkedin_url', 'label' => 'Linkedin Url', 'section' => 'footer_settings', 'default' => ''),
+		array('name'=> 'github_url', 'label' => 'Github Url', 'section' => 'footer_settings', 'default' => ''),
+		array('name'=> 'other_media_url', 'label' => 'Other Media Url', 'section' => 'footer_settings', 'default' => ''),
+		array('name'=> 'other_media2_url', 'label' => 'Other Media Two Url', 'section' => 'footer_settings', 'default' => ''),
+    );
+
+	foreach($url_inputs  as $url){
+        add_url_input($wp_customize, $url['name'], $url['label'], $url['section'], $url['default']);
+    }
+
+	$images = array(
+        
+		array('name' => 'instagram_icon', 'label' => 'Social Media Icon - Instagram', 'section' => 'footer_settings', 'default' => ''),
+		array('name'=> 'linkedin_icon', 'label' => 'Linkedin Icon', 'section' => 'footer_settings', 'default' => ''),
+		array('name'=> 'github_icon', 'label' => 'Github Icon', 'section' => 'footer_settings', 'default' => ''),
+		array('name'=> 'other_media_icon', 'label' => 'Other Media Icon', 'section' => 'footer_settings', 'default' => ''),
+		array('name'=> 'other_media2_icon', 'label' => 'Other Media Two Icon', 'section' => 'footer_settings', 'default' => '')
+	);
+
+	foreach($images  as $image){
+        add_image($wp_customize, $image['name'], $image['label'], $image['section']);
+    }
+
+
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
@@ -59,3 +90,33 @@ function jacobs_portfolio_customize_preview_js() {
 	wp_enqueue_script( 'jacobs-portfolio-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
 }
 add_action( 'customize_preview_init', 'jacobs_portfolio_customize_preview_js' );
+
+
+function add_url_input($wp_customize, $name, $label, $section, $default = '') {
+    $wp_customize->add_setting($name,
+        array(
+            'default' => $default,
+            'sanitize_callback' => 'esc_url_raw'
+        )
+    );
+
+    $wp_customize->add_control($name,
+        array(
+            'label' => $label,
+            'section' => $section,
+            'settings' => $name,
+            'type' => 'url'
+        )
+    );
+}
+
+function add_image($wp_customize, $name, $label, $section){
+    $wp_customize->add_setting($name, array(
+    ));
+
+    $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, $name , array(
+        'label' => $label,
+        'settings'  => $name,
+        'section'   => $section
+    ) ));
+}
