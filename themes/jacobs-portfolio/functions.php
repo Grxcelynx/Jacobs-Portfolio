0b1d3378db6a8e35df3547cf13f3be2935c35772
Grace Birnam <grace.mbirnam@gmail.com>
2023-02-09 12:38:30 -0800

    small fix

--- themes/jacobs-portfolio/functions.php
+++ themes/jacobs-portfolio/functions.php
@@ -340,3 +340,10 @@ function theme_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+add_filter( 'oembed_response_data', 'disable_embeds_filter_oembed_response_data_' );
+function disable_embeds_filter_oembed_response_data_( $data ) {
+    unset($data['author_url']);
+    unset($data['author_name']);
+    return $data;
+}
